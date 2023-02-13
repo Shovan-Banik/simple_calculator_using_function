b1c8ffe95adb9ef8859c78c32faa485db22b08db
Shovan-Banik <shovan15-7790@diu.edu.bd>
2023-02-14 03:14:01 +0600

    calculator done

--- calculation.js
+++ calculation.js
@@ -13,12 +13,50 @@ function elementValue(string) {
 }
 function setValue(id, result) {
     document.getElementById(id).innerText=result;
+
+    // aivabeo kora jay
+
+    // const finalResult=document.getElementById(id);
+    // finalResult.innerText=result;
 }
+
+// function GetTwoInput(first,second){
+//     const first = inputValue('first-input');
+//     const second = inputValue('second-input');
+//     return (first,second);
+// }
 
 
 document.getElementById('sum').addEventListener('click', function () {
     const firstInput = inputValue('first-input');
     const secondInput = inputValue('second-input');
+    // GetTwoInput(firstInput,secondInput);
     const summation = firstInput + secondInput;
     setValue('final-output',summation);
+})
+document.getElementById('sub').addEventListener('click',function(){
+    const firstInput = inputValue('first-input');
+    const secondInput = inputValue('second-input');
+    // GetTwoInput(firstInput,secondInput);
+    const subtraction = firstInput - secondInput;
+    setValue('final-output',subtraction);
+})
+document.getElementById('mul').addEventListener('click',function(){
+    const firstInput = inputValue('first-input');
+    const secondInput = inputValue('second-input');
+    // GetTwoInput(firstInput,secondInput);
+    const multiplication = firstInput * secondInput;
+    setValue('final-output',multiplication);
+})
+document.getElementById('div').addEventListener('click',function(){
+    const firstInput = inputValue('first-input');
+    const secondInput = inputValue('second-input');
+    if(firstInput<secondInput){
+        alert('First number should be big');
+    }else{
+        const division = firstInput / secondInput;
+        setValue('final-output',division);
+    }
+    // GetTwoInput(firstInput,secondInput);
+    
 })
